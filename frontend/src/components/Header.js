4d8071faeabbf37945e3c4c6866f242bc47bee65
AbhijitSarkaar <theabhijitsarkar@gmail.com>
2022-06-16 07:38:50 +0530

    routing and product screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,38 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="#home">ProShop</Navbar.Brand>
+                    <Link to={"/"}>
+                        <Navbar.Brand>ProShop</Navbar.Brand>
+                    </Link>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            <Nav.Link href="/cart">Cart</Nav.Link>
-                            <Nav.Link href="/login">Sign In</Nav.Link>
+                            <Link to={"/cart"}>
+                                <div
+                                    style={{
+                                        color: "white",
+                                        marginLeft: "20px",
+                                    }}
+                                >
+                                    Cart
+                                </div>
+                            </Link>
+                            <Link to={"/login"}>
+                                <div
+                                    style={{
+                                        color: "white",
+                                        marginLeft: "20px",
+                                    }}
+                                >
+                                    Sign In
+                                </div>
+                            </Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
