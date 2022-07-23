d531e6f168a31857e8d8f4c98076e19ce1ff1953
AbhijitSarkaar <theabhijitsarkar@gmail.com>
2022-07-23 12:56:45 +0530

    procfile and config changes

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,6 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("API is running");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -33,6 +29,16 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "frontend", "build", "index.html")
+        );
+    });
+}
+
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 //route not found error handler
